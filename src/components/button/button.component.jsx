0ea7e70de7f422196f1e76145f1dbcfe6f951d1e
mcraeven <mcraevenangelo@gmail.com>
2022-06-18 23:59:50 +0800

    conifigured stripe payment method

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -2,6 +2,7 @@ import {
   BaseButton,
   InvertedButton,
   GoogleSignInButton,
+  ButtonSpinner,
 } from "./button.styles.jsx";
 
 export const BUTTON_TYPE = {
@@ -17,9 +18,9 @@ const getButton = (buttonType = BUTTON_TYPE.base) =>
     [BUTTON_TYPE.google]: GoogleSignInButton,
   }[buttonType]);
 
-const Button = ({ children, buttonType, ...attributes }) => {
+const Button = ({ children, buttonType, isLoading, ...attributes }) => {
   const CustomButton = getButton(buttonType);
-  return <CustomButton {...attributes}>{children}</CustomButton>;
+  return <CustomButton disabled={isLoading} {...attributes}>{ isLoading ? <ButtonSpinner /> : children}</CustomButton>;
 };
 
 export default Button;
--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { SpinnerContainer } from "../spinner/spinner.styles";
+
 export const BaseButton = styled.button`
   min-width: 165px;
   width: auto;
@@ -11,12 +13,12 @@ export const BaseButton = styled.button`
   background-color: black;
   color: white;
   text-transform: uppercase;
-
   font-weight: bolder;
   border: none;
   cursor: pointer;
   display: flex;
   justify-content: center;
+  align-items: center;
 
   &:hover {
     background-color: white;
@@ -46,3 +48,8 @@ export const InvertedButton = styled(BaseButton)`
     border: none;
   }
 `;
+
+export const ButtonSpinner = styled(SpinnerContainer)`
+  width:30px;
+  height:30px;
+`
